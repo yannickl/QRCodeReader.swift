af695aebc6563ee8ef0f686ac77e976c4d08958f
Yannick Loriot <yannick.loriot@gmail.com>
2015-09-17 21:45:47 +0200

    [ADD] Carthage Support

---
+++ Example/QRCodeReader/QRCodeReader.h
@@ -0,0 +1,19 @@
+//
+//  QRCodeReader.h
+//  QRCodeReader
+//
+//  Created by Yannick LORIOT on 17/09/15.
+//  Copyright © 2015 Yannick Loriot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QRCodeReader.
+FOUNDATION_EXPORT double QRCodeReaderVersionNumber;
+
+//! Project version string for QRCodeReader.
+FOUNDATION_EXPORT const unsigned char QRCodeReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRCodeReader/PublicHeader.h>
+
+
