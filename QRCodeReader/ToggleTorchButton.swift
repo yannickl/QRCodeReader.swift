893cca17b2bcafb79c91cf060bbdee7b45595d84
Yurii Zadoianchuk <y.zadoianchuk@thenewmotion.com>
2015-10-01 09:53:35 +0200

    Adds options for hiding switch camera button and toggling torch on/off

--- QRCodeReader/ToggleTorchButton.swift
+++ QRCodeReader/ToggleTorchButton.swift
@@ -34,42 +34,104 @@ import UIKit
         }
     }
     
-    @IBInspectable var fillColor: UIColor  = UIColor.darkGrayColor() {
+    @IBInspectable var fillColor: UIColor  = UIColor.lightGrayColor() {
         didSet {
             setNeedsDisplay()
         }
     }
     
     @IBInspectable var edgeHighlightedColor: UIColor = UIColor.whiteColor()
-    @IBInspectable var fillHighlightedColor: UIColor = UIColor.blackColor()
+    @IBInspectable var fillHighlightedColor: UIColor = UIColor.darkGrayColor()
     
     override func drawRect(rect: CGRect) {
-        UIColor.whiteColor().setFill()
-        UIColor.yellowColor().setStroke()
+        // Colors
+        let paintColor  = (self.state != .Highlighted) ? fillColor : fillHighlightedColor
+        paintColor.setFill()
+        let strokeColor = (self.state != .Highlighted) ? edgeColor : edgeHighlightedColor
+        strokeColor.setStroke()
         
         let width  = rect.width
         let height = rect.height
-        let center = width / 2
-        let middle = height / 2
+        let centerX = width / 2
+        let centerY = height / 2
         
-        let strokeLineWidth = CGFloat(2)
-        let circleRadius = width / 10
-        let lineLength = width / 7
-        let lineOffset = width / 14
+        let strokeLineWidth: CGFloat = 2
+        let circleRadius: CGFloat = width / 10
+        let lineLength: CGFloat = width / 10
+        let lineOffset: CGFloat = width / 10
+        let lineOriginFromCenter = circleRadius + lineOffset
+        let sin45 = sin(CGFloat(M_PI_4))
+        let inclinedLength = lineLength * sin45
+        let inclinedOrigin = lineOriginFromCenter * sin45
         
+        //Circle
         let circlePath = UIBezierPath()
-        circlePath.addArcWithCenter(CGPoint(x: center, y: middle), radius: circleRadius, startAngle: 0.0, endAngle: CGFloat(M_PI), clockwise: true)
-        circlePath.addArcWithCenter(CGPoint(x: center, y: middle), radius: circleRadius, startAngle: CGFloat(M_PI), endAngle: CGFloat(M_PI * 2), clockwise: true)
+        circlePath.addArcWithCenter(CGPoint(x: centerX, y: centerY), radius: circleRadius, startAngle: 0.0, endAngle: CGFloat(M_PI), clockwise: true)
+        circlePath.addArcWithCenter(CGPoint(x: centerX, y: centerY), radius: circleRadius, startAngle: CGFloat(M_PI), endAngle: CGFloat(M_PI * 2), clockwise: true)
         
-        let linePath = UIBezierPath()
-        linePath.moveToPoint(CGPointMake(center, middle + circleRadius + lineOffset))
-        linePath.addLineToPoint(CGPointMake(center, middle + circleRadius + lineOffset + lineLength))
-        linePath.lineWidth = strokeLineWidth
-        linePath.stroke()
+        //First beam
+        var startPoint = CGPoint(x: centerX, y: centerY + lineOriginFromCenter)
+        var endPoint = CGPoint(x: startPoint.x, y: startPoint.y + lineLength)
+        let firstBeamPath = linePathWithStartPoint(startPoint, endPoint: endPoint, thickness: strokeLineWidth)
+        firstBeamPath.stroke()
+        
+        //Second beam
+        startPoint = CGPoint(x: centerX + sin45 * lineOriginFromCenter, y: centerY + inclinedOrigin)
+        endPoint = CGPoint(x: startPoint.x + inclinedLength, y: startPoint.y + inclinedLength)
+        let secondBeamPath = linePathWithStartPoint(startPoint, endPoint: endPoint, thickness: strokeLineWidth)
+        secondBeamPath.stroke()
+        
+        //Third beam
+        startPoint = CGPoint(x: centerX + lineOriginFromCenter, y: centerY)
+        endPoint = CGPoint(x: startPoint.x + lineLength, y: startPoint.y)
+        let thirdBeamPath = linePathWithStartPoint(startPoint, endPoint: endPoint, thickness: strokeLineWidth)
+        thirdBeamPath.stroke()
+        
+        //Fourth beam
+        startPoint = CGPoint(x: centerX + inclinedOrigin, y: centerY - inclinedOrigin)
+        endPoint = CGPoint(x: startPoint.x + inclinedLength, y: startPoint.y - inclinedLength)
+        let fourthBeamPath = linePathWithStartPoint(startPoint, endPoint: endPoint, thickness: strokeLineWidth)
+        fourthBeamPath.stroke()
+        
+        //Fifth beam
+        startPoint = CGPoint(x: centerX, y: centerY - lineOriginFromCenter)
+        endPoint = CGPoint(x: startPoint.x, y: startPoint.y - lineLength)
+        let fifthBeamPath = linePathWithStartPoint(startPoint, endPoint: endPoint, thickness: strokeLineWidth)
+        fifthBeamPath.stroke()
+        
+        //Sixth beam
+        startPoint = CGPoint(x: centerX - inclinedOrigin, y: centerY - inclinedOrigin)
+        endPoint = CGPoint(x: startPoint.x - inclinedLength, y: startPoint.y - inclinedLength)
+        let sixthBeamPath = linePathWithStartPoint(startPoint, endPoint: endPoint, thickness: strokeLineWidth)
+        sixthBeamPath.stroke()
+        
+        //Seventh beam
+        startPoint = CGPoint(x: centerX - lineOriginFromCenter, y: centerY)
+        endPoint = CGPoint(x: startPoint.x - lineLength, y: startPoint.y)
+        let seventhBeamPath = linePathWithStartPoint(startPoint, endPoint: endPoint, thickness: strokeLineWidth)
+        seventhBeamPath.stroke()
+        
+        //Eights beam
+        startPoint = CGPoint(x: centerX - inclinedOrigin, y: centerY + inclinedOrigin)
+        endPoint = CGPoint(x: startPoint.x - sin45 * lineLength, y: startPoint.y + inclinedLength)
+        let eightsBeamPath = linePathWithStartPoint(startPoint, endPoint: endPoint, thickness: strokeLineWidth)
+        eightsBeamPath.stroke()
+        
         
         circlePath.lineWidth = strokeLineWidth
         circlePath.fill()
         circlePath.stroke()
+    }
+    
+    private func linePathWithStartPoint(startPoint: CGPoint, endPoint: CGPoint, thickness: CGFloat) -> UIBezierPath {
+        let linePath = UIBezierPath()
+        
+        linePath.moveToPoint(startPoint)
+        linePath.addLineToPoint(endPoint)
+        linePath.lineCapStyle = .Round
+        linePath.lineWidth = thickness
+        
+        return linePath
     }
     
     // MARK: - UIResponder Methods
