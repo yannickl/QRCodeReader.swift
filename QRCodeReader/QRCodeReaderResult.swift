f0242d4aeb7f028eeca7860d0ccbcc1a9ac5bf61
Yannick Loriot <yannick.loriot@gmail.com>
2016-01-12 11:13:16 +0100

    [UPDATE] Example project

--- QRCodeReader/QRCodeReaderResult.swift
+++ QRCodeReader/QRCodeReaderResult.swift
@@ -26,7 +26,13 @@
 
 import Foundation
 
+/**
+ The result of the scan with its content value and the corresponding metadata type.
+*/
 public struct QRCodeReaderResult {
+  /**
+   The error corrected data decoded into a human-readable string.
+   */
   public let value: String
 
   /**
