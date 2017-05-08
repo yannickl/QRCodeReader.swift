4245cca10aee8bac513fe1d896699e79aa0f821f
Yannick Loriot <yannick.loriot@gmail.com>
2017-05-08 19:24:40 +0200

    Adding the preview layer example

--- Example/QRCodeReader.swift/ViewController.swift
+++ Example/QRCodeReader.swift/ViewController.swift
@@ -24,11 +24,13 @@
  *
  */
 
-import UIKit
 import AVFoundation
+import UIKit
 
 class ViewController: UIViewController, QRCodeReaderViewControllerDelegate {
-  lazy var reader: QRCodeReaderViewController = {
+  @IBOutlet weak var previewView: UIView!
+  lazy var reader: QRCodeReader = QRCodeReader()
+  lazy var readerVC: QRCodeReaderViewController = {
     let builder = QRCodeReaderViewControllerBuilder {
       $0.reader = QRCodeReader(metadataObjectTypes: [AVMetadataObjectTypeQRCode], captureDevicePosition: .back)
       $0.showTorchButton = true
@@ -37,46 +39,72 @@ class ViewController: UIViewController, QRCodeReaderViewControllerDelegate {
     return QRCodeReaderViewController(builder: builder)
   }()
 
-  @IBAction func scanAction(_ sender: AnyObject) {
+  // MARK: - Actions
+
+  private func checkScanPermissions() -> Bool {
     do {
-      if try QRCodeReader.supportsMetadataObjectTypes() {
-        reader.modalPresentationStyle = .formSheet
-        reader.delegate               = self
-
-        reader.completionBlock = { (result: QRCodeReaderResult?) in
-          if let result = result {
-            print("Completion with result: \(result.value) of type \(result.metadataType)")
-          }
-        }
-
-        present(reader, animated: true, completion: nil)
-      }
+      return try QRCodeReader.supportsMetadataObjectTypes()
     } catch let error as NSError {
+      let alert: UIAlertController?
+
       switch error.code {
       case -11852:
-        let alert = UIAlertController(title: "Error", message: "This app is not authorized to use Back Camera.", preferredStyle: .alert)
+        alert = UIAlertController(title: "Error", message: "This app is not authorized to use Back Camera.", preferredStyle: .alert)
 
-        alert.addAction(UIAlertAction(title: "Setting", style: .default, handler: { (_) in
+        alert?.addAction(UIAlertAction(title: "Setting", style: .default, handler: { (_) in
           DispatchQueue.main.async {
             if let settingsURL = URL(string: UIApplicationOpenSettingsURLString) {
               UIApplication.shared.openURL(settingsURL)
             }
           }
         }))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        present(alert, animated: true, completion: nil)
 
-
-
+        alert?.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
       case -11814:
-        let alert = UIAlertController(title: "Error", message: "Reader not supported by the current device", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        alert = UIAlertController(title: "Error", message: "Reader not supported by the current device", preferredStyle: .alert)
+        alert?.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+      default:
+        alert = nil
+      }
 
-        present(alert, animated: true, completion: nil)
-      default:()
+      guard let vc = alert else { return false }
+
+      present(vc, animated: true, completion: nil)
+
+      return false
+    }
+  }
+
+  @IBAction func scanInModalAction(_ sender: AnyObject) {
+    guard checkScanPermissions() else { return }
+
+    readerVC.modalPresentationStyle = .formSheet
+    readerVC.delegate               = self
+
+    readerVC.completionBlock = { (result: QRCodeReaderResult?) in
+      if let result = result {
+        print("Completion with result: \(result.value) of type \(result.metadataType)")
       }
     }
 
+    present(readerVC, animated: true, completion: nil)
+  }
+
+  @IBAction func scanInPreviewAction(_ sender: Any) {
+    guard checkScanPermissions(), !reader.isRunning else { return }
+
+    reader.previewLayer.frame = previewView.bounds
+    previewView.layer.addSublayer(reader.previewLayer)
+
+    reader.startScanning()
+    reader.didFindCode = { result in
+      let alert = UIAlertController(
+        title: "QRCodeReader",
+        message: String (format:"%@ (of type %@)", result.value, result.metadataType),
+        preferredStyle: .alert
+      )
+      alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+    }
   }
 
   // MARK: - QRCodeReader Delegate Methods
